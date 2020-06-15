4d1258ba232eb299ad8ddf1ef5415b9742df2a1e
liuwang <330141830@qq.com>
2020-06-15 19:54:27 +0800

    第二章完结

---
+++ 2-chapter/fromscikit.py
@@ -0,0 +1,88 @@
+from sklearn.linear_model import Perceptron
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+
+# pandas是专门为处理表格和混杂数据设计的，而NumPy更适合处理统一的数值数组数据。
+
+
+# load data
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['label'] = iris.target
+
+df.columns = [
+    'sepal length', 'sepal width', 'petal length', 'petal width', 'label'
+]
+print(df.label.value_counts())
+
+plt.scatter(df[:50]['sepal length'], df[:50]['sepal width'], label='0')
+plt.scatter(df[50:100]['sepal length'], df[50:100]['sepal width'], label='1')
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.legend()
+plt.show()
+
+
+data = np.array(df.iloc[:100, [0, 1, -1]])
+X, y = data[:, :-1], data[:, -1]
+y = np.array([1 if i == 1 else -1 for i in y])
+
+clf = Perceptron(fit_intercept=True,
+                 max_iter=1000,
+                 shuffle=True)
+clf.fit(X, y)
+
+# 画布大小
+plt.figure(figsize=(10,10))
+
+# 中文标题
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+plt.title('鸢尾花线性数据示例')
+
+plt.scatter(data[:50, 0], data[:50, 1], c='b', label='Iris-setosa',)
+plt.scatter(data[50:100, 0], data[50:100, 1], c='orange', label='Iris-versicolor')
+
+# 画感知机的线
+x_ponits = np.arange(4, 8)
+y_ = -(clf.coef_[0][0]*x_ponits + clf.intercept_)/clf.coef_[0][1]
+plt.plot(x_ponits, y_)
+
+# 其他部分
+plt.legend()  # 显示图例
+plt.grid(False)  # 不显示网格
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.legend()
+
+
+clf = Perceptron(fit_intercept=True,
+                 max_iter=1000,
+                 tol=None,
+                 shuffle=True)
+clf.fit(X, y)
+
+# 画布大小
+plt.figure(figsize=(10,10))
+
+# 中文标题
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+plt.title('鸢尾花线性数据示例')
+
+plt.scatter(data[:50, 0], data[:50, 1], c='b', label='Iris-setosa',)
+plt.scatter(data[50:100, 0], data[50:100, 1], c='orange', label='Iris-versicolor')
+
+# 画感知机的线
+x_ponits = np.arange(4, 8)
+y_ = -(clf.coef_[0][0]*x_ponits + clf.intercept_)/clf.coef_[0][1]
+plt.plot(x_ponits, y_)
+
+# 其他部分
+plt.legend()  # 显示图例
+plt.grid(False)  # 不显示网格
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.legend()
